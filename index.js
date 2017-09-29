621426df80cf29ee9ed7067c7772c894b66104f2
Dan Daly <dan.john.daly@gmail.com>
2017-09-29 19:50:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
